94ecd0884fbc1cb7d6afba674e3e58313d14547d
Nick <nickeb96@gmail.com>
2020-02-19 16:14:11 -0500

    Hard coded color hex values to bypass terminal defaults

--- conf.d/nautilus_colors.fish
+++ conf.d/nautilus_colors.fish
@@ -1,24 +1,44 @@
-set -g fish_color_autosuggestion 555
+
+# Prefered Color Pallet:
+#
+#   black       '0x000000'
+#   red         '0xd54e53'
+#   green       '0xb9ca4a'
+#   yellow      '0xe6c547'
+#   blue        '0x7aa6da'
+#   magenta     '0xc397d8'
+#   cyan        '0x70c0ba'
+#   white       '0xeaeaea'
+#   brblack     '0x666666'
+#   brred       '0xff3334'
+#   brgreen     '0x9ec400'
+#   bryellow    '0xe7c547'
+#   brblue      '0x7aa6da'
+#   brmagenta   '0xb77ee0'
+#   brcyan      '0x54ced6'
+#   brwhite     '0xffffff'
+
+set -g fish_color_autosuggestion 555555
 set -g fish_color_cancel --reverse
-set -g fish_color_command green
-set -g fish_color_comment brblack
-set -g fish_color_cwd --bold blue
-set -g fish_color_cwd_root --bold red
-set -g fish_color_end brcyan
-set -g fish_color_error brred
-set -g fish_color_escape --bold bryellow
+set -g fish_color_command b9ca4a
+set -g fish_color_comment 666666
+set -g fish_color_cwd --bold 7aa6da
+set -g fish_color_cwd_root --bold d54e53
+set -g fish_color_end 54ced6
+set -g fish_color_error ff3334
+set -g fish_color_escape --bold e7c547
 set -g fish_color_history_current --bold
-set -g fish_color_host brgreen
-set -g fish_color_match --background=brblue
+set -g fish_color_host 9ec400
+set -g fish_color_match --background=7aa6da
 set -g fish_color_normal normal
-set -g fish_color_operator brmagenta
+set -g fish_color_operator b77ee0
 set -g fish_color_param normal
-set -g fish_color_quote yellow
-set -g fish_color_redirection brcyan
-set -g fish_color_search_match bryellow --background=brblack
-set -g fish_color_selection white --bold --background=brblack
-set -g fish_color_status red
-set -g fish_color_user brblue
+set -g fish_color_quote e6c547
+set -g fish_color_redirection 54ced6
+set -g fish_color_search_match e7c547 --background=666666
+set -g fish_color_selection eaeaea --bold --background=666666
+set -g fish_color_status d54e53
+set -g fish_color_user 7aa6da
 set -g fish_color_valid_path --underline
 
 
@@ -36,3 +56,4 @@ function $uninstall_event --on-event $uninstall_event
         set -eg $var
     end
 end
+
