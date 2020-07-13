4df5c4f350a6f73e8b834f5ba6b3a7d709459317
nickeb96 <nickeb96@gmail.com>
2020-07-13 11:22:43 -0400

    Added dir stack indicator

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -3,4 +3,7 @@ function fish_right_prompt --description 'Write out the right prompt'
     if [ $status -ne 0 ]
         echo -ns -- (set_color red)"<$last_status>"(set_color normal)
     end
+    if count $dirstack >/dev/null
+        echo -ns -- (set_color magenta)"*"(set_color normal)
+    end
 end
