38c98244890e40b0819eb2028f4e99146e9dcdb6
nickeb96 <nickeb96@gmail.com>
2020-11-17 14:05:29 -0500

    Change cat to only use fold in interactive shells

--- functions/cat.fish
+++ functions/cat.fish
@@ -1,15 +1,20 @@
-function cat --wraps=cat
-    set -l markdown false
-    for arg in $argv
-        if string match -q -- "*.md" $arg
-            set markdown true
-        else
-            set markdown false
-            break
+function cat --wraps=cat --description 'Fold markdown files instead of cat when interactive.'
+    if status is-interactive
+        # if all input files are markdown, use fold instead
+        set -l markdown false
+        for arg in $argv
+            if string match -q -- "*.md" $arg
+                set markdown true
+            else
+                set markdown false
+                break
+            end
         end
-    end
-    if $markdown
-        command fold -s $argv
+        if $markdown
+            command fold -s $argv
+        else
+            command cat $argv
+        end
     else
         command cat $argv
     end
