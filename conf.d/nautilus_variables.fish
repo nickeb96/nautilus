1a6d226b6e0027adba965afe9529ba826d03ed4a
nickeb96 <nickeb96@gmail.com>
2022-10-27 16:44:25 -0400

    Added default variables for rsync_options and fish_prompt_*

--- conf.d/nautilus_variables.fish
+++ conf.d/nautilus_variables.fish
@@ -1,8 +1,16 @@
 set -q pisces_only_insert_at_eol
 or set -g pisces_only_insert_at_eol 1
 
+set -q fish_prompt_pwd_dir_length
+or set -g fish_prompt_pwd_dir_length 4
+
+set -q fish_prompt_pwd_use_ellipsis
+or set -g fish_prompt_pwd_use_ellipsis yes
+
+set -q rsync_options
+or set -g rsync_options --times --crtimes --open-noatime --perms --owner --group --links --recursive
 
 set -l uninstall_event (basename (status filename) .fish)_uninstall
-function $uninstall_event --on-event $uninstall_event
+function _$uninstall_event --on-event $uninstall_event
     # no op for now
 end
